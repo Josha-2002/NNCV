7e6230ce76158fb6adc37878be9cda974ed94b9c
Josha <j.j.j.kuipers@student.tue.nl>
2026-03-26 19:20:33 +0100

    origina lr and lower weights

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -2,12 +2,12 @@ wandb login
 
 python3 train.py \
     --data-dir ./data/cityscapes \
-    --batch-size 8 \
-    --epochs 10 \
-    --lr 0.00001 \
+    --batch-size 16 \
+    --epochs 20 \
+    --lr 0.00006 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "segformer-b0-cityscape-512x1024-v6-Weighted"
+    --experiment-id "segformer-b0-cityscape-512x1024-v6-Lower_Weighted"
 
 
 # wandb login
