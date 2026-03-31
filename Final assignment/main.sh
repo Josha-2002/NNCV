4b62f1a394e60e4e6fc2d2bf7609438178f083ef
Josha <j.j.j.kuipers@student.tue.nl>
2026-03-31 18:43:05 +0200

    checking with 256x512 unweighted

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -7,7 +7,7 @@ python3 train.py \
     --lr 0.00006 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "segformer-b1-384x768-UnWeighted"
+    --experiment-id "segformer-b1-256x512-UnWeighted"
 
 
 # wandb login
